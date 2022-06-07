da4804b6314cfc125cc7d14d76cefe37fb8285e0
dylan <dylan@successit.com.sg>
2022-06-07 21:06:19 +0800

    Shorten solution

--- leetcode/Two Sum/solution_1.cpp
+++ leetcode/Two Sum/solution_1.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> ans;
         unordered_map<int, int> om;
         for (int i = 0; i < size(nums); ++i) {
             if (om.find(target - nums[i]) != om.end()) {
-                ans.push_back(om[target - nums[i]]);
-                ans.push_back(i);
-                return ans;
+                return {om[target - nums[i]], i};
             }
             
             om[nums[i]] = i;
         }
         
-        return ans;
+        return {};
     }
 };
