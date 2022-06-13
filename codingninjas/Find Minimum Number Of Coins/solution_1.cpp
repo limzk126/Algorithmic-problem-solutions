52c485866dcdf25d120f77b94e7e3f2e742f16ae
dylan <dylanlim12698@gmail.com>
2022-06-14 01:09:09 +0800

    Upload solution

---
+++ codingninjas/Find Minimum Number Of Coins/solution_1.cpp
@@ -0,0 +1,16 @@
+int findMinimumCoins(int amount) 
+{
+    vector<int> coins = {1, 2, 5, 10, 20, 50, 100, 500 , 1000};
+    
+    int num_coins = 0;
+    int i = coins.size() - 1;
+    while (amount) {
+        while (amount >= coins[i]) {
+            amount -= coins[i];
+            ++num_coins;
+        }
+        --i;
+    }
+    
+    return num_coins;
+}
