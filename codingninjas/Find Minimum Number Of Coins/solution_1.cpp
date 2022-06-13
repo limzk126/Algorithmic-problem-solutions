e44b7b48a880c838061cce4f7d98e96344b571c0
dylan <dylanlim12698@gmail.com>
2022-06-14 01:11:26 +0800

    Fix whitespace formatting

--- codingninjas/Find Minimum Number Of Coins/solution_1.cpp
+++ codingninjas/Find Minimum Number Of Coins/solution_1.cpp
@@ -1,6 +1,6 @@
 int findMinimumCoins(int amount) 
 {
-    vector<int> coins = {1, 2, 5, 10, 20, 50, 100, 500 , 1000};
+    vector<int> coins = {1, 2, 5, 10, 20, 50, 100, 500, 1000};
     
     int num_coins = 0;
     int i = coins.size() - 1;
