fc082a59d86a0b8b34f1f3a2e2177b25d0ade42f
puzzlehead <dylanlim12698@gmail.com>
2022-07-12 23:38:12 +0800

    Upload solution

---
+++ leetcode/Binary Tree Inorder Traversal/solution_3.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+
+        TreeNode* cur = root;
+
+        while (cur != nullptr) {
+            if (cur->left == nullptr) {
+                res.push_back(cur->val);
+                cur = cur->right;
+            } else {
+                TreeNode* last = cur->left;
+                while (last->right && last->right != cur) {
+                    last = last->right;
+                }
+
+                if (last->right == nullptr) {
+                    last->right = cur;
+                    cur = cur->left;
+                    continue;
+                }
+
+                if (last->right == cur) {
+                    last->right = nullptr;
+                    res.push_back(cur->val);
+                    cur = cur->right;
+                }
+            }
+        }
+
+        return res;
+    }
+};
