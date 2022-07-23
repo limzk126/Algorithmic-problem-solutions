cd01106b3a05a26863266d9dcdbeff88047794d2
puzzlehead <dylanlim12698@gmail.com>
2022-07-23 22:45:39 +0800

    Upload solution

---
+++ leetcode/Binary Search Tree Iterator/solution_2.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class BSTIterator {
+    public:
+        stack<TreeNode*> s;
+
+        BSTIterator(TreeNode* root) {
+            pushAll(root);        
+        }
+
+        int next() {
+            TreeNode* node = s.top();
+            s.pop();
+            pushAll(node->right);
+            return node->val;
+        }
+
+        bool hasNext() {
+            return !s.empty(); 
+        }
+
+    private:
+        void pushAll(TreeNode* node) {
+            while (node) {
+                s.push(node);
+                node = node->left;
+            }
+        }
+};
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
