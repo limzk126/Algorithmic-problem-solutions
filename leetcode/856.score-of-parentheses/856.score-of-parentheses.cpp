adda33da186d973a6a5b0a633a7629e391e9e201
puzzlehead <dylanlim12698@gmail.com>
2022-08-26 21:37:56 +0800

    Upload solution

---
+++ leetcode/856.score-of-parentheses/856.score-of-parentheses.cpp
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=856 lang=cpp
+ *
+ * [856] Score of Parentheses
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int scoreOfParentheses(string S)
+    {
+        stack<int> stack;
+        int cur = 0;
+        for (char i : S)
+        {
+            if (i == '(')
+            {
+                stack.push(cur);
+                cur = 0;
+            }
+            else
+            {
+                cur += stack.top() + max(cur, 1);
+                stack.pop();
+            }
+        }
+        return cur;
+    }
+};
+// @lc code=end
